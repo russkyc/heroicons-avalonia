e78bf4c1281aee2318cd38ff8541da08b9a64c11
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-11-17 15:40:00 +0800

    :tada: Begin project

---
+++ HeroIconsAvalonia/Controls/HeroIcon.axaml.cs
@@ -0,0 +1,102 @@
+﻿// MIT License
+// 
+// Copyright (c) 2023 Russell Camo (Russkyc)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using HeroIconsAvalonia.Enums;
+
+namespace HeroIconsAvalonia.Controls;
+
+public partial class HeroIcon : UserControl
+{
+    public HeroIcon()
+    {
+        SetSize(Min);
+        InitializeComponent();
+    }
+    
+    public static readonly StyledProperty<bool> MinProperty = AvaloniaProperty.Register<HeroIcon, bool>(
+        "Min");
+
+    public bool Min
+    {
+        get => GetValue(MinProperty);
+        set
+        {
+            SetValue(MinProperty, value);
+            SetSize(value);
+        }
+    }
+    
+    void SetSize(bool min)
+    {
+        Width = min ? 20 : 24;
+        Height = min ? 20 : 24;
+    }
+
+    static readonly StyledProperty<object?> IconSourceProperty = AvaloniaProperty.Register<HeroIcon, object?>(
+        "IconSource");
+    
+    object? IconSource
+    {
+        set => SetValue(IconSourceProperty, value);
+    }
+
+    public static readonly StyledProperty<Enums.IconType> TypeProperty = AvaloniaProperty.Register<HeroIcon, Enums.IconType>(
+        "Type");
+
+    public Enums.IconType Type
+    {
+        get => GetValue(TypeProperty);
+        set
+        {
+            SetValue(TypeProperty, value);
+            var resource = Resources.MergedDictionaries[(int)Kind] as ResourceDictionary;
+            IconSource = resource![Type.ToString()];
+        }
+    }
+
+    public static readonly StyledProperty<IconKind> KindProperty = AvaloniaProperty.Register<HeroIcon, IconKind>(
+        "Kind");
+
+    public IconKind Kind
+    {
+        get => GetValue(KindProperty);
+        set
+        {
+            SetValue(KindProperty, value);
+            Type = Type;
+        }
+    }
+    
+    public new IBrush? Foreground
+    {
+        get => GetValue(ForegroundProperty);
+        set
+        {
+            SetValue(ForegroundProperty, value);
+            var resource = Resources.MergedDictionaries[(int)Kind] as ResourceDictionary;
+            resource!["Brush0"] = value;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+﻿<img src=".github/resources/images/banner.svg" style="width: 100%;" />
+
+<h2 align="center">HeroIcons.Avalonia</h2>
+
+<p style="text-align: justify">The beautiful handcrafted Icons by the makers of Tailwind CSS
+made available to your awesome Avalonia UI projects!
+</p>
+
+<h4 align="center">HeroIcons.Avalonia Demo App</h4>
+
+<img src=".github/resources/images/demo.png" style="width: 100%;" />
+
+### How to Use
+
+### Donate
+
+If you find this project helpful, please consider donating. It helps me afford more time to create tools like this
+for the community. ❤
+
+### Special Message
+Special thanks to [JetBrains](https://www.jetbrains.com/) for supporting this project by providing licences to the JetBrains Suite!
+
+<a href="https://www.jetbrains.com/community/opensource/#support">
+<img width="200px" src="https://resources.jetbrains.com/storage/products/company/brand/logos/jb_beam.png" alt="JetBrains Logo (Main) logo.">
+</a>
